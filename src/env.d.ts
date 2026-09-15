c335779da9b338f7d67153a200dd66bf805546a5
vmrosti <vojtech.razdik@gmail.com>
2026-09-15 14:55:58 +0200

    Delete src directory

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="astro/client" />
-
-interface Window {
-  trackEvent?: (name: string, props?: Record<string, unknown>) => void;
-  plausible?: (name: string, options?: { props?: Record<string, unknown> }) => void;
-}
